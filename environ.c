7afe82a72657a90a6c4a87c6376e84c1aa554748
Tyrone Joel <125516735+Tyjay00@users.noreply.github.com>
2023-05-26 22:48:50 +0200

    Update environ.c

--- environ.c
+++ environ.c
@@ -1,10 +1,9 @@
 #include "shell.h"
 
 /**
-<<<<<<< HEAD
- * populate_env_list - function populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * populate_env_list - fills in the env linked list
+ * @info: Containing a potential argument structure. used to keep
+ *         a constant function prototype.
  * Return: Always 0
  */
 int populate_env_list(info_t *info)
@@ -19,10 +18,7 @@ int populate_env_list(info_t *info)
 }
 
 /**
- * _myunsetenv - function removes an environment variable
-=======
- * _myunsetenv - function removes an environment variable
->>>>>>> 32e1edc381d1512888bc6188065fd5457e664757
+ * _myunsetenv - Remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: Always 0
@@ -43,31 +39,7 @@ int _myunsetenv(info_t *info)
 }
 
 /**
-<<<<<<< HEAD
- * _mysetenv - function initializes a new environment variable,
-=======
- * populate_env_list - function populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
- */
-
-int populate_env_list(info_t *info)
-
-{
-
-	list_t *node = NULL;
-	size_t i;
-
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
-	return (0);
-}
-
-/**
- * _mysetenv - function initializes a new environment variable,
->>>>>>> 32e1edc381d1512888bc6188065fd5457e664757
+ * _mysetenv - Initialize a new environment variable,
  *             or modify an existing one
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
@@ -86,19 +58,7 @@ int _mysetenv(info_t *info)
 }
 
 /**
- * _myenv - function prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
- */
-int _myenv(info_t *info)
-{
-	print_list_str(info->env);
-	return (0);
-}
-
-/**
- * _getenv - function gets the value of an environ variable
+ * _getenv - gets the value of an environ variable
  * @info: Structure containing potential arguments. Used to maintain
  * @name: env var name
  *
@@ -117,4 +77,16 @@ char *_getenv(info_t *info, const char *name)
 		node = node->next;
 	}
 	return (NULL);
+}
+
+/**
+ * _myenv - prints the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int _myenv(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
 }
