dfc70095591b714323eb697251fb3faf17e43259
Zwivhuya Muthathi <88210910+Vhazee@users.noreply.github.com>
2023-05-26 22:09:18 +0200

    Update realloc.c

--- realloc.c
+++ realloc.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * _realloc - reallocates a block of memory
+ * _realloc - function reallocates a block of memory
  * @ptr: pointer to previous malloc'ated block
  * @old_size: byte size of previous block
  * @new_size: byte size of new block
@@ -31,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 }
 
 /**
- **_memset - fills memory with a constant byte
+ **_memset - function fills memory with a constant byte
  *@s: the pointer to the memory area
  *@b: the byte to fill *s with
  *@n: the amount of bytes to be filled
@@ -47,7 +47,7 @@ char *_memset(char *s, char b, unsigned int n)
 }
 
 /**
- * ffree - frees a string of strings
+ * ffree - function frees a string of strings
  * @pp: string of strings
  */
 void ffree(char **pp)
