0986f7ca95f40cddfca281b538e67bb49fb6d7dc
Tyrone <tyronejoel04@gmail.com>
2023-05-26 14:59:45 -0700

    Simple shell

--- getenv.c
+++ getenv.c
@@ -1,47 +1,9 @@
 #include "shell.h"
 
 /**
-<<<<<<< HEAD
- * _setenv - Initialize a new environment variable,
-=======
- * _unsetenv - function remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: 1 on delete, 0 otherwise
- * @var: the string env var property
- */
-
-int _unsetenv(info_t *info, char *var)
-
-{
-	list_t *node = info->env;
-	size_t i = 0;
-	char *p;
-
-	if (!node || !var)
-		return (0);
-
-	while (node)
-	{
-		p = starts_with(node->str, var);
-		if (p && *p == '=')
-		{
-			info->env_changed = delete_node_at_index(&(info->env), i);
-			i = 0;
-			node = info->env;
-			continue;
-		}
-		node = node->next;
-		i++;
-	}
-	return (info->env_changed);
-}
-
-/**
- * _setenv - function initialize a new environment variable,
->>>>>>> 32e1edc381d1512888bc6188065fd5457e664757
+ * _setenv - create a new environment variable from scratch
  *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: Arrangement with potential arguments. Used to maintain
  *        constant function prototype.
  * @var: the string env var property
  * @value: the string env var value
@@ -82,7 +44,7 @@ int _setenv(info_t *info, char *var, char *value)
 }
 
 /**
- * get_environ - function returns the string array copy of our environ
+ * get_environ - returns a duplicate of our environment's string array.
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
@@ -99,8 +61,8 @@ char **get_environ(info_t *info)
 }
 
 /**
- * _unsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
+ * _unsetenv - Eliminate a environment variable
+ * @info: Arrangement with possible arguments. Used to maintain
  *        constant function prototype.
  *  Return: 1 on delete, 0 otherwise
  * @var: the string env var property
